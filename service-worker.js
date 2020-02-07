4bd546ea211534f06ee3dbfad0c8c1e10d0164aa
Dolly Chhabriya <dolly.c@altius.cc>
2020-02-07 16:04:08 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/faspdeveloper/dollySprint1.git/precache-manifest.a6100cde6be0138a3ab5c0c70526b68f.js"
+  "/dollySprint1.git/precache-manifest.8bab6a60788f77df7dad4e56d1c0e5c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/faspdeveloper/dollySprint1.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dollySprint1.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
